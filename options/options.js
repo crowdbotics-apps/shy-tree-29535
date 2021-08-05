f5be2e31997342ddaa848573a6ef4a83b6e9a46a
Crowdbotics <team@crowdbotics.com>
2021-08-05 21:44:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "shy_tree_29535Identifier",
+  url: "https://shy_tree_29535Identifier.botics.co",
+  api: "https://shy_tree_29535Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
